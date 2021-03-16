647265f65af78d35278ec1038c109ce93a8f6067
aminebenjamel8@gmail.com <AMINEBAYOULI20>
2021-03-16 14:02:53 +0100

    add methode search

--- src/app/Amine/amine.app.component.ts
+++ src/app/Amine/amine.app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {UserseviceService} from './service/usersevice.service';
 import {MatDialog,MatDialogConfig} from '@angular/material/dialog'
 import { NewuserComponent } from './newuser/newuser.component';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'amine-app',
@@ -10,13 +12,17 @@ import { NewuserComponent } from './newuser/newuser.component';
 })
 export class AmineAppComponent { 
   constructor(private userservice : UserseviceService,private dialog:MatDialog){}
-  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'email'];
-  dataSource = this.userservice.ELEMENT_DATA;
+  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'email','actions'];
+  dataSource =  new MatTableDataSource( this.userservice.ELEMENT_DATA);
   oncreate(){
     const DialogConfig = new MatDialogConfig();
     DialogConfig.disableClose = true;
     DialogConfig.autoFocus = true;
     DialogConfig.width ="60%";
     this.dialog.open(NewuserComponent);
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
